13563632af69ced227b8e8d865887bd44ac19373
Yuda Kaito <211019@st.yoshida-g.ac.jp>
2022-10-05 17:27:38 +0900

    fade.cppの命名規則の整理。

--- fade.h
+++ fade.h
@@ -1,25 +1,33 @@
-//============================
+//=============================================================================
 //
-//  フェード
-// Author:hamada ryuuga
+// フェード
+// Author : Hamada Ryuuga
 //
-//============================
+//=============================================================================
 #ifndef _FADE_H_
 #define _FADE_H_
 
+//-----------------------------------------------------------------------------
+// include
+//-----------------------------------------------------------------------------
 #include "manager.h"
 #include "renderer.h"
 #include "object2d.h"
 
+//-----------------------------------------------------------------------------
+// 前方宣言
+//-----------------------------------------------------------------------------
 class  CMotion;
 
+//=============================================================================
+// フェードクラス
+//=============================================================================
 class CFade : public CObject2d
 {
 
 public:
 	CFade();
 	~CFade();
-
 
 	//画面(モード)の種類
 	enum FADE
@@ -30,20 +38,20 @@ public:
 		FADEMAX
 	};
 
-	HRESULT Init()override;	// 初期化
-	void Uninit()override;	// 破棄
-	void Update()override;	// 更新
-	void Draw()override;	// 描画
+	HRESULT Init() override;
+	void Uninit() override;
+	void Update() override;
+	void Draw() override;
+
 	static CFade* Create();
 	void CFade::NextMode(CManager::MODE nextMode);
-	FADE * GetFade() { return&fade; };
+	FADE* GetFade() { return& fade; }
+
 private:
-
-	float m_FadeSp;
-	float m_FadeSet;
-	bool m_Bake;
-	CManager::MODE m_NextMode;
+	float m_fadeSp;
+	float m_fadeSet;
+	bool m_bake;
+	CManager::MODE m_nextMode;
 	FADE fade;
-
 };
 #endif
